3b98ce8e726e018e3eb3187b9b4518af65b17d37
Travis Martin <tjmartin2003@gmail.com>
2025-10-20 15:08:34 -0500

    `@ts-expect-error` for global stylesheet imports (which don't have types).

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+// @ts-expect-error Global stylesheets don't have types.
 import "styles/global.scss";
 import type { Metadata, Viewport } from "next";
 import { monospace, sansSerif, serif } from "util/font";
